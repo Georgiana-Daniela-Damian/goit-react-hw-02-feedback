ed9bc28545d5317872e79e20e5d6cc7b7cca4365
Georgiana-Daniela-Damian <danutza89tm@gmail.com>
2024-08-23 19:44:35 +0300

    update components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Feedback from './Feedback/Feedback';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = e => {
+    const currentBtnValue = e.currentTarget.value;
+    this.setState(prevState => ({
+      ...prevState,
+      [currentBtnValue]: prevState[currentBtnValue] + 1,
+    }));
+  };
+
+  countFeedback = () => {
+    const valuesArr = Object.values(this.state);
+    return valuesArr.reduce((acc, val) => {
+      acc += val;
+      return acc;
+    });
+  };
+  countZero = () => {
+    if (
+      this.state.good === 0 &&
+      this.state.neutral === 0 &&
+      this.state.bad === 0
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  countPercentage = () => {
+    return Math.round((this.state.good / this.countFeedback()) * 100);
+  };
+
+  render() {
+    const buttons = Object.keys(this.state);
+    return (
+      <div className="App">
+        <section>
+          <Feedback options={buttons} leaveFeedback={this.leaveFeedback} />
+          {this.countZero() ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              state={this.state}
+              total={this.countFeedback()}
+              positivePercentage={this.countPercentage()}
+            />
+          )}
+        </section>
+      </div>
+    );
+  }
+}
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,17 @@
+import style from './Notification.module.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Notification extends Component {
+  static defaultProps = {
+    message: ' ',
+  };
+  static propTypes = {
+    Message: PropTypes.string,
+  };
+  render() {
+    return <h2 className={style.notification}>{this.props.message}</h2>;
+  }
+}
+
+export default Notification;
